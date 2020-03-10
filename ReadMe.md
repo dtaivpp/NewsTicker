f361aff0ddcaa7f6b85ba7064a787d023a5aba3d
David Tippett <17506770+dtaivpp@users.noreply.github.com>
2020-03-10 10:36:05 -0400

    added scrape page framework and readme

---
+++ ReadMe.md
@@ -0,0 +1,3 @@
+# NewsTicker
+
+News ticker is a tool to track the public opinion of a company. It will use several data sources (Twitter, News, etc.) to gather how the public feels about certain companies. It will then plot these onto either a box and wisker chart or a line chart. 
---
+++ src/scrape_page.py
@@ -0,0 +1,9 @@
+
+
+def page_scrape(url):
+    # Pull out the content of the page
+    pass
+
+if __name__=="__main__":
+    page_scrape("https://www.bloomberg.com/news/articles/2020-03-09/stock-rout-to-continue-in-asia-after-u-s-plunge-markets-wrap?srnd=premium")
+    page_scrape("https://finance.yahoo.com/news/why-cheering-the-crash-in-oil-prices-like-trump-just-did-is-oversimplifying-the-risks-203530179.html")
