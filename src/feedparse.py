a10b20f354853819d1570ff8489dd090ac988cfa
David Tippett <17506770+dtaivpp@users.noreply.github.com>
2020-03-11 17:10:23 -0400

    Updating to be more modular

--- src/feedparse.py
+++ src/feedparse.py
@@ -7,13 +7,22 @@ hit_list = [ "https://rss.nytimes.com/services/xml/rss/nyt/US.xml",
              "https://feeds.a.dj.com/rss/RSSWSJD.xml",
              "https://seekingalpha.com/feed.xml" ] # list of feeds to pull down
 
-# pull down all feeds
-future_calls = [Future(feedparser.parse,rss_url) for rss_url in hit_list]
-# block until they are all in
-feeds = [future_obj() for future_obj in future_calls]
+def query_rss():
+    '''Query All RSS Feeds'''
+    # pull down all feeds
+    future_calls = [Future(feedparser.parse,rss_url) for rss_url in hit_list]
+    # block until they are all in
+    feeds = [future_obj() for future_obj in future_calls]
 
-entries = []
-for feed in feeds:
-    entries.extend( feed[ "items" ] )
+    entries = []
+    for feed in feeds:
+        entries.extend( feed[ "items" ] )
+    
+    update_db(entries)
 
-print(len(entries))
+def update_db(recordset):
+    '''Find which articles are new and send to db'''
+    pass
+
+if __name__=="__main__":
+    query_rss()
