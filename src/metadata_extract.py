461a528cb32cd7f3f6f743c28e61d2ddeaec1a7b
David Tippett <17506770+dtaivpp@users.noreply.github.com>
2020-03-12 16:08:58 -0400

    finished metadata extraction from og metadata tags

--- src/metadata_extract.py
+++ src/metadata_extract.py
@@ -2,16 +2,20 @@ from bs4 import BeautifulSoup
 import lxml
 
 def grab_meta(request):
-    '''Create a json metadata object from meta tags'''
+    '''Create a json metadata object from request object'''
     page = request.text
     soup = BeautifulSoup(page, 'lxml')
 
     meta_list = soup.findAll('meta')
 
-    for tag in meta_list:
-        if "og:" in tag["property"]:
-            print( tag["property"] )
+    metadata = {}
 
+    for tag in meta_list:
+        if "property" in tag.attrs:
+            if "og:" in tag.attrs["property"]:
+                metadata[tag.attrs["property"]] = tag.attrs["content"]
+
+    return metadata
 
 if __name__=="__main__":
     # Load up test pages
@@ -19,5 +23,5 @@ if __name__=="__main__":
     
     request_list = get_test_requests()
 
-    for page in pages_list:
+    for page in request_list:
         grab_meta(page)
