9643cbf90858960293d6bcab5350b7a22c95631d
David Tippett <17506770+dtaivpp@users.noreply.github.com>
2020-03-12 13:04:51 -0400

    Created to load test pages

---
+++ src/util/load_test_page.py
@@ -0,0 +1,25 @@
+import os
+import pickle
+
+def get_test_requests():
+    '''Returns a list of response objects'''
+
+    # Iterate pages in test_data folder
+    testdir = os.path.join(os.curdir,'test_data')
+    request_files = []
+    
+    # iterate over all test docs
+    for filename in os.listdir(testdir):
+        file_parts = os.path.splitext(filename)
+
+        # only retrieve .page files
+        if file_parts[len(file_parts)-1] == '.page':
+            path = os.path.join(testdir, filename)
+            with open(path, 'rb') as openfile:
+                request = pickle.load(openfile)
+                request_files.append(page)
+
+    return request_files
+
+if __name__ == "__main__":
+    pass
