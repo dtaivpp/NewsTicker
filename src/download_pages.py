2608af7a95fb2e80f781e29e772db04593644896
David Tippett <17506770+dtaivpp@users.noreply.github.com>
2020-03-16 16:58:41 -0400

    added page save error, updated func names

--- src/download_pages.py
+++ src/download_pages.py
@@ -6,7 +6,7 @@ from datetime import datetime
 from util.request_tools import get_random_ua, extract_domain
 
 
-def generate_headers():
+def _generate_headers():
     '''Build Headers with Random UA'''
     user_agent = get_random_ua()
     headers = {
@@ -14,7 +14,7 @@ def generate_headers():
     }
     return headers
 
-def generate_filename(url):
+def _generate_filename(url):
     '''Create a filename for the path formatted domain + datetime'''
     cwd = os.getcwd()
     domain_name = extract_domain(url)
@@ -23,13 +23,39 @@ def generate_filename(url):
     filename = filename.replace(':', '_')
     return filename + ".page"
 
+
+class WriteToFileError(Exception):
+    def __init__(self, *args):
+        if args:
+            self.message = args[0]
+        else:
+            self.message = None
+    
+    def __str__(self):
+        if self.message:
+            return f"Could not write to file: {self.message}"
+        else:
+            return "Write to file error"
+
+
+def _save_page(request):
+    outfile = _generate_filename(url)
+    try:
+        with open(outfile, 'wb+') as pickle_file:
+            pickle.dump(response, pickle_file)
+    except:
+        raise WriteToFileError(outfile)
+
+
 def page_download(url):
     '''Download and pickle the page'''
     # Pull out the content of the page
-    response = requests.get(url, headers=generate_headers())
-    outfile = generate_filename(url)
-    with open(outfile, 'wb+') as pickle_file:
-        pickle.dump(response, pickle_file)
+    response = requests.get(url, headers=_generate_headers())
+    
+    try:
+        _save_page(response)
+    except SavePageError:
+        print(SavePageError)
 
 
 if __name__=="__main__":
