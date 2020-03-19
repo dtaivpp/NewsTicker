5b6bce94e48b3b257fade1c7694af8423171695f
David Tippett <17506770+dtaivpp@users.noreply.github.com>
2020-03-19 13:24:43 -0400

    Lots of typo fixes. Need to add main test

--- src/util/load_scrape_config.py
+++ src/util/load_scrape_config.py
@@ -1,17 +1,18 @@
 import json
+import os
 
 def _config_reader(file_path):
     
     config_json = {}
-    with open(config_path, 'r') as config_file:
+    with open(file_path, 'r') as config_file:
         config_json = json.load(config_file)
     
     return config_json
 
-def get_scrape_config()
-    config_path = os.path.join(os.curdir,'src', 'config', 'scrape.config')
+def get_scrape_config():
+    config_path = os.path.join(os.curdir,'src', 'config', 'scrape_config.json')
     return _config_reader(config_path)
 
-def get_rss_config()
+def get_rss_config():
     config_path = os.path.join(os.curdir,'src', 'config', 'rss.config')
     return _config_reader(config_path)
