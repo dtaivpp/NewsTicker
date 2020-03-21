0eda8bef5d29fbc9284fda5a158dc9bb4fc9a321
David Tippett <17506770+dtaivpp@users.noreply.github.com>
2020-03-21 12:37:21 -0400

    Replaced old scrape logic with new logic

--- src/scrape_page.py
+++ src/scrape_page.py
@@ -1,58 +1,116 @@
 from bs4 import BeautifulSoup
 import lxml
-import os
-import json
-from util.domain_tools import extract_domain
+from util.load_scrape_config import get_scrape_config
+from util.request_tools import extract_domain
 
-valid_descendants = {'p', 'li', 'td', 'a'}
+# --------------------------------------------------
+# WIP New Page Scraping File Will Replace scrape_page.py
+# 1. Find Outer Most Element
+# 2. Locate wanted inner elements
+# 3. Remove unwanted elements
+# 4. Extract article text
+#
+# Extraction Model:
+#  {
+#   root_element: ["tag", {"attribute": "attribute-value"}],
+#   unwanted_elements: [
+#                      ["tag", {"attribute": "attribute-value"}],
+#                      ["tag", {"attribute": "attribute-value"}]
+#                     ],
+#   inner_elements:[
+#                   ["tag", {"attribute": "attribute-value"}],
+#                   ["tag", {"attribute": "attribute-value"}]
+#                  ],
+#   text_elements: [
+#                   ["tag", {"attribute": "attribute-value"}],
+#                   ["tag", {"attribute": "attribute-value"}]
+#                  ]
+#   }
+# --------------------------------------------------
 
-def recusive_search(root_element):
-    '''Find the text values of all article elements'''
-    for descendent in root_element.descendants:
-        if descendent.name in valid_descendants:
-            # Need a better way to isolate releated elements? 
-            # Possibly a specific scrape per site? 
-            print(descendent.text)
+# Get outer element
+def outer_element(page, identifyer):
+    root = page.find(*identifyer)
+    
+    if root == None:
+        raise Exception("Could not find root element")
+
+    return root
+
+# Remove unwanted elements
+def trim_unwanted_elements(page, identifier_list):
+    # Check if list has elements
+    if len(identifier_list) != 0:
+        for identifyer in identifier_list:
+            for element in page.find_all(*identifyer):
+                element.decompose()
+    
+    # Dont ask why I am doing this. 
+    #   I know this doesnt actually
+    #   do anything as I am referencing
+    #   the page object...
+    return page
+
+# Extract text
+def get_text(page, identifier_list):
+    # Check if list has elements
+    if len(identifier_list) == 0:
+        raise Exception("Get text needs to know which elements to extract.")
+
+    page_text = []
+    for identifyer in identifier_list:
+        for element in page.find_all(*identifyer):
+            page_text.append(element.text)
+    
+    return page_text
 
 
-
-def element_to_target(url):
-    '''Returns an object with parameters used to extract the article text'''
-    config_data = {}
-
-    configpath = os.path.join(os.curdir,'src','config', 'scrape.config')
-    with open(configpath, 'r') as config_handle:
-        config_data = json.load(config_handle)
-
-    domain_name = extract_domain(url)
-
-    if domain_name in config_data:
-        return config_data[domain_name]
-
-    return None
+# Get page config
+def load_scrape_config():
+    '''Loads page scraping config data'''
+    return get_scrape_config()
 
 
+def get_site_config(url):
+    '''Get the scrap config for the site'''
+    domain = extract_domain(url)
 
+    config_data = load_scrape_config()
+    
+    config = config_data.get(domain, None)
+    
+    if config == None:
+        raise Exception(f"Config does not exist for domiain: {domain}")
+    
+    return config
+
+# Build Soup
 def page_processer(request):
     '''Returns Article Text'''
-    # Pull out the content of the page
-    article_identifier = element_to_target(request.url)
 
+    # Get the page scrape config
+    site_config = get_site_config(request.url)
+
+    # Soupify page
     soup = BeautifulSoup(request.text, 'lxml')
     
-    # Query config file for type of identifier needed
-    root_article_element = soup.find(article_identifier)
+    # Retrieve root element
+    root = outer_element(soup, site_config["root_element"])
 
-    text = recusive_search(root_article_element)
+    trimmed_tree = trim_unwanted_elements(root, site_config["unwanted_elements"])
+
+    text = get_text(trimmed_tree, site_config["text_elements"])
         
-    return text
+    return " ".join(text)
 
 
 if __name__=="__main__":
     # Load up test pages
-    from util.load_test_page import get_test_requests
+    from util.load_test_data import get_test_pages
     
-    request_list = get_test_requests()
+    request_list = get_test_pages()
 
     for page in request_list:
-        page_processer(page)
+        text = page_processer(page)
+        print(text)
+        print("".join(['--'for i in range(20)])
